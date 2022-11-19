87168e9b278c622452ef2687540e22b7ba7d31d3
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-19 08:18:28 +0600

    Issue Simple JWT token

--- doctors-portal-server/index.js
+++ doctors-portal-server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -121,6 +122,20 @@ async function run() {
       }
       const result = await bookingsCollections.insertOne(booking);
       res.send(result);
+    });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollections.findOne(query);
+      // console.log(user);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(401).send({ accessToken: "unauthorized access" });
     });
 
     app.post("/users", async (req, res) => {
